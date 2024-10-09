690971bb1554a7221505517b722bb48311d7e501
AngelVelasco256 <ahernandezv1808@alumno.ipn.mx>
2024-10-08 22:12:55 -0600

    Agregado el archivo de Operadores de Comparacion

---
+++ operadoresComparacion.js
@@ -0,0 +1,24 @@
+//Operadores de Comparacion
+//==
+console.log('8' == 8);
+
+//=== *recomendado Compara el valor y el tipo de dato
+console.log('8' === 8);
+
+//!=
+console.log('8' != 8);
+
+//!== *recomendado Compara el valor y el tipo de dato
+console.log('8' !== 8);
+
+//<
+console.log(7 < 5);
+
+//>
+console.log(7 > 5);
+
+//<= Menor o igual
+console.log(8 <= 8);
+
+//>= Mayor o igual
+console.log(10 >= 10);
