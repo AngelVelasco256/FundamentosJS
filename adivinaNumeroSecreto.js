aa8188f775d4fc58b3a6d29f9f269913034ae7a4
AngelVelasco256 <ahernandezv1808@alumno.ipn.mx>
2024-10-12 00:01:04 -0600

    Agregado el archivo de juego Adivina el numero secreto, incluido terminal

---
+++ adivinaNumeroSecreto.js
@@ -0,0 +1,20 @@
+// let numeroSecreto  = Math.floor(Math.random()  * 10);
+// let numeroTecleado = parseInt(prompt('¿Puedes predecir el numero secreto?'));
+
+// if (numeroTecleado === numeroSecreto) {
+//   alert(`Felicidades, el numero ${numeroTecleado} es correcto`);
+// } else {
+//   alert(`Lo sentimos, el numero ${numeroTecleado} no es correcto, Intenta nuevamente`)
+// }
+
+//EJERCICIO EN TERMINAL
+process.stdout.write('dime el numero: ');
+process.stdin.on('data', function(data){
+  let numeroTecleado = data;
+  let numeroSecreto = Math.floor(Math.random() * 10);
+  if (numeroTecleado == numeroSecreto) {
+    process.stdout.write(`Felicidades, el numero ${numeroTecleado} es correcto \n`);
+  } else {
+    process.stdout.write(`Lo sentimos, el numero ${numeroTecleado} es incorrecto, intenta nuevamente \n`);
+  }
+})
