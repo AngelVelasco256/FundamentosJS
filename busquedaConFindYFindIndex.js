cd0de09b252209e86fb70692f0f8afa80c60401e
AngelVelasco256 <ahernandezv1808@alumno.ipn.mx>
2024-10-30 23:14:40 -0600

    Agregado el archivo de Busqueda con Find y FindIndex

---
+++ busquedaConFindYFindIndex.js
@@ -0,0 +1,12 @@
+//FIND
+//
+const multiplosDeCinco = [5, 10, 15]
+const numeroMayorADiez = multiplosDeCinco.find(number => number >= 10)
+console.log(numeroMayorADiez)
+
+
+
+//FINDINDEX
+const multiplosDeCinco = [5, 10, 15]
+const numeroMayorADiez = multiplosDeCinco.findIndex(number => number >= 12)
+console.log(numeroMayorADiez)
