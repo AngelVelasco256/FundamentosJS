4337096dde9f97bfebc23c29fbea557cf5bdf62a
AngelVelasco256 <ahernandezv1808@alumno.ipn.mx>
2024-11-15 22:07:26 -0600

    Repaso de metodo Push y Pop

--- modificacionFinalPushAndPop.js
+++ modificacionFinalPushAndPop.js
@@ -1,12 +1,13 @@
 //MUTABILIDAD (MODIFICAR EL ARREGLO ORIGINAL)
-//PUSH() Agrega los elementos enviados como parametro al FINAL de nuestro array original y devuelve su longitud
+//PUSH() Agrega los elementos enviados como parametro al FINAL de nuestro array original y devuelve UNICAMENTE su longitud
 const paises = ['México', 'España', 'Japón', 'Brasil']
-const newPaises = paises.push('Corea del Sur', 'Argentina', 'Alemania')
+const newPaises = paises.push('Corea del Sur', 'Argentina', 'Alemania') //El metodo push() agrega AL FINAL DEL ARRAY los elementos que pasemos como parametros
 
-console.log(paises);
-console.log(newPaises);
+console.log(paises); //>>['México', 'España', 'Japón', 'Brasil', 'Corea del Sur', 'Argentina', 'Alemania']
+console.log(newPaises); //La variable newPaises solo almacenara lo que retorna el metodo push(), un number que refleja la nueva longitud del array
 
 //POP() elimina el ultimo elemento del array y devuelve el elemento que fue removido
-const eliminarPaises = paises.pop()
-console.log(paises);
-console.log(eliminarPaises);
+const paises = ['México', 'España', 'Japón', 'Brasil']
+const eliminarPaises = paises.pop() //Al ejecutar el metodo .pop() se elimina el ultimo elemento ('Brasil') y lo devuelve
+console.log(paises) //>>['México', 'España', 'Japón', 'Brasil']
+console.log(eliminarPaises) //>> Brasil
