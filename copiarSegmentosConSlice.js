287161ee2aef54eff6d88039cd96d023f70fab64
AngelVelasco256 <ahernandezv1808@alumno.ipn.mx>
2024-11-23 19:12:54 -0600

    Repaso de Slice

--- copiarSegmentosConSlice.js
+++ copiarSegmentosConSlice.js
@@ -1,16 +1,19 @@
 //SLICE()
 const palabras = ["sol", "luna", "estrella", "cielo", "mar", "montaña", "río", "bosque", "viento", "fuego", "tierra", "nieve", "lluvia", "nube", "trueno"];
+console.log(palabras.length)
 
-//Slice(1 parametro) indica el comienzo a partir del cual copiara el contenido del array original
+//Slice(1 parámetro) indica el comienzo a partir del cual copiara el contenido del array original
 console.log(palabras.slice(2))
 
-//Slice(1, 2 parametros) señala el indice de inicio y el indice del final de la copia El segundo parametro no incluye el valor del index, si no uno antes, poreso se recomienda calcular i+1
-console.log(palabras.slice(4, 9 + 1))
+//Slice(1, 2 parámetros) señala el indice de inicio y el indice del final de la copia El segundo parámetro no incluye el valor del index, si no uno antes, por eso se recomienda calcular i+1
+console.log(palabras.slice(1, 4))
+console.log(palabras.slice(1, 4 + 1))
 
-//Slice (-1 parametro) Con los numeros negativos el array comenzara a copiar del ultimo elemento hacia atras y se detendra hasta el numero en negativo que indique el index
+//Slice (-1 parámetro) Con un parámetro indicado como negativo tomara el ultimo valor del array original como -1 y comenzara a contar hasta el valor -i indicado como parámetro y comenzara a copiar a partir de ahi y hasta el final
 console.log(palabras.slice(-3))
-//Aqui comienza el index en un numero positivo pero el final se indica en el numero 3 del final del array hacia atras sin olvidar que para que se muestre el final podemos calcular i+1
+
+//Aquí comienza el index en un numero positivo pero 6el final se indica en el numero 3 del final del array hacia atrás sin olvidar que para que se muestre el final podemos calcular i+1
 console.log(palabras.slice(5, -3+1))
 
-//Slice sin parametros copiara todo el array pero en uno nuevo SIN MODIFICAR el original
+//Slice sin parámetros copiara todo el array pero en uno nuevo SIN MODIFICAR el original
 console.log(palabras.slice())
